a8eb83873bbcee1d24a1dbe2ab161acc5ba9ea82
alexandermahone12 <abdullahsawaftah07@protonmail.com>
2023-02-05 11:27:10 +0300

    logo change
    logo changed from medicinenee to mymedicine

--- src/pages/relaiability.js
+++ src/pages/relaiability.js
@@ -5,8 +5,8 @@ import "@fontsource/manrope";
 const Relaiability = () => {
   return (
     <div className = "h1reliable">
-      <h1>We use evicence based resources (Up To Date database) as a source for all of the information on our page. We have made sure that all the used sources are 100% reliable and we made sure to cite them here:</h1>
-      <button>Sources</button>
+      <h1>We use evicence based resources (UpToDate.com database) as a source for all of the information on our page. We have made sure that all the used sources are 100% reliable and we made sure to cite them here:</h1>
+      <h2>www.uptodate.com</h2>
     </div>
   );
 };
